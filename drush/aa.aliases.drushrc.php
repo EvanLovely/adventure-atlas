4c829163a0ebbcf307f77804c9d21bca4f5f9a37
Evan Lovely <evan@evanlovely.com>
2016-04-06 13:33:39 -0700

    updating ip

--- drush/aa.aliases.drushrc.php
+++ drush/aa.aliases.drushrc.php
@@ -1,8 +1,8 @@
 <?php
 
 $aliases['prod'] = array(
-    'uri' => 'http://104.236.143.228',
-    'remote-host' => '104.236.143.228',
+    'uri' => 'http://45.55.28.8',
+    'remote-host' => '45.55.28.8',
     'root' => '/var/www/html/drupal',
     'remote-user' => 'root',
 );
