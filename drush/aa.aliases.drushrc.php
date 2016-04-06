4c97ec184cc3381363fd27c459810abf067821fe
Evan Lovely <evan@evanlovely.com>
2016-04-06 11:15:07 -0700

    adding remote site alias

---
+++ drush/aa.aliases.drushrc.php
@@ -0,0 +1,8 @@
+<?php
+
+$aliases['prod'] = array(
+    'uri' => 'http://104.236.143.228',
+    'remote-host' => '104.236.143.228',
+    'root' => '/var/www/html/drupal',
+    'remote-user' => 'root',
+);
