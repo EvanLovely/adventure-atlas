bddb67fc97adf6e13b075287d52e0d567316b0d9
Evan Lovely <evan@evanlovely.com>
2016-04-06 13:49:26 -0700

    adjusting drush path

--- drush/aa.aliases.drushrc.php
+++ drush/aa.aliases.drushrc.php
@@ -3,6 +3,9 @@
 $aliases['prod'] = array(
     'uri' => 'http://45.55.28.8',
     'remote-host' => '45.55.28.8',
-    'root' => '/var/www/html/drupal',
+    'root' => '/var/www/adventure-atlas/web',
     'remote-user' => 'root',
+    'path-aliases' => array(
+      '%drush-script' => '/var/www/adventure-atlas/vendor/drush/drush/drush'
+    )
 );
